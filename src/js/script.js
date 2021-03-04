64b3eac4b39080bc99f38e2977f21ba8b4a5f26a
Maxim Kozlov <Maximusvin@gmail.com>
2021-03-04 18:18:29 +0200

    init project

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+//js modules
+
+import './preloader';
+import './firebase';
+import './language';
+import './trending-page';
+import './MovieDetailsModal';
+import './search';
+import './modalRegistration';
+import './components/header/menu';
+import './ourTeam';
+import './pagination';
+import './mainScrollUp';
---
+++ webpack/index.js
@@ -0,0 +1,8 @@
+import 'normalize.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
+import './scss/main.scss';
+
+//main JS
+import './js/script.js';
+
+
