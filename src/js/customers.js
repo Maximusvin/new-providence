11dbcbc58169db1ee97e483371474c1bf88f4625
Maxim Kozlov <Maximusvin@gmail.com>
2021-03-14 23:26:45 +0200

    fix slider

--- src/js/customers.js
+++ src/js/customers.js
@@ -1,16 +1,33 @@
 import Swiper from './../../node_modules/swiper';
-  // import Swiper styles
+// import Swiper styles
 import './../../node_modules/swiper/swiper-bundle.css';
 
-const swiper = new Swiper('.swiper-container',{
-    
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+SwiperCore.use([Navigation, Pagination]);
+
+const imgRef = document.querySelector('.swiper-pagination');
+const imgArr = imgRef.children;
+
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    type: 'custom',
+    bulletClass: 'swiper_pagination_img',
+    bulletActiveClass: 'swiper_pagination_img-active',
+    renderCustom: function (swiper, current, total) {
+      imgArr.forEach(item => {
+        console.log(item);
+        item.classList.remove('swiper_pagination_img-active');
+      });
+      imgArr[current - 1].classList.add('swiper_pagination_img-active');
+      console.dir(imgRef.children);
     },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
