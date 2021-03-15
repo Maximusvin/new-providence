8f98514a2a906a7a7165ad751e2704f71df9da2a
T.Tiana <t.tiana@keemail.me>
2021-03-15 04:34:05 +0200

    interface block done

--- src/js/interface.js
+++ src/js/interface.js
@@ -5,10 +5,6 @@ const INTERFACE = new Swiper('.interface__slider', {
   slidesPerView: 'auto',
   centeredSlides: true,
   spaceBetween: 0,
-  loop: true,
-  pagination: {
-    el: '.interface__slider-pagination',
-    clickable: true,
-  }
+  loop: true
 });
 
