c865c603ca219d0bc5ae4c1b0de47f18444c76f8
Yuliia Havryliuk <gavriliukyulichka@gmail.comgit config --global user.email gavriliukyulichka@gmail.com>
2021-03-16 15:36:23 +0200

    Added adaptive on header

--- src/js/header.js
+++ src/js/header.js
@@ -6,7 +6,6 @@ const infoListRef = document.querySelector('.header__info-list')
 const headerLogoRef = document.querySelector('.header__logo')
 const headerBtnRef = document.querySelector('.header__info-btn')
 const headerCloseBtn = document.querySelector('.header__close-btn')
-const initialWidth = window.innerWidth
 
 const windowResize = window.addEventListener('resize',event=>{
     if(event.target.innerWidth<=710){
@@ -33,6 +32,28 @@ const windowResize = window.addEventListener('resize',event=>{
             headerCloseBtn.classList.remove('active')
         })
     }else if(event.target.innerWidth>710){
-        console.log(`The window is bigger than 710px!`)
-    }
+        burgerRef.removeEventListener('click',() =>{
+            console.log('YES!')
+            burgerRef.classList.add('active')
+            burgerRef.classList.add('hidden')
+            headerMenuRef.classList.add('active')
+            headerNavigationRef.classList.add('active')
+            menuListRef.classList.add('active')
+            infoListRef.classList.add('active')
+            headerLogoRef.classList.add('active')
+            headerBtnRef.classList.add('hidden')
+            headerCloseBtn.classList.add('active')
+        })
+        headerCloseBtn.removeEventListener('click',()=>{
+            burgerRef.classList.remove('active')
+            burgerRef.classList.remove('hidden')
+            headerMenuRef.classList.remove('active')
+            headerNavigationRef.classList.remove('active')
+            menuListRef.classList.remove('active')
+            infoListRef.classList.remove('active')
+            headerLogoRef.classList.remove('active')
+            headerBtnRef.classList.remove('hidden')
+            headerCloseBtn.classList.remove('active')
+    })
+}
 })
