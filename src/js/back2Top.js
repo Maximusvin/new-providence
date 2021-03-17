d8abae950735c730b27ab5d65b0adf72bf1c1e90
Maxim Kozlov <Maximusvin@gmail.com>
2021-03-17 12:29:18 +0200

    added button toUp

---
+++ src/js/back2Top.js
@@ -0,0 +1,22 @@
+const back2TopRef = document.querySelector('back2Top');
+
+import debounce from 'lodash/debounce';
+const container = document.querySelector('html');
+
+const mainScrollUp = document.querySelector('.main-scrollUp');
+
+const handleScrollUpBtnClick = () => {
+  container.scrollTop = 0;
+  console.log('click');
+};
+
+const scrollFunction = () => {
+  if (container.scrollTop > 20) {
+    mainScrollUp.style = 'opacity:1';
+  } else {
+    mainScrollUp.style = 'opacity:0';
+  }
+};
+
+window.addEventListener('scroll', debounce(scrollFunction, 150));
+mainScrollUp.addEventListener('click', handleScrollUpBtnClick);
